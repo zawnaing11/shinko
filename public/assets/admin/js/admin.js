ff9fe8abf4d88e73999175f0c17e0ee12dce9c68
nakasone-abridge <anakasone@abridge-co.jp>
2024-06-02 08:42:31 +0900

    #2 管理画面テンプレートインストール

---
+++ public/assets/admin/js/admin.js
@@ -0,0 +1,52 @@
+$(document).on('submit', '.form-store', function() {
+    if (!confirm('情報を登録してもよろしいですか？')) {
+        return false;
+    }
+});
+
+$(document).on('submit', '.form-update', function() {
+    if (!confirm('情報を更新してもよろしいですか？')) {
+        return false;
+    }
+});
+
+$(document).on('submit', '.form-destroy', function() {
+    if (!confirm('削除してもよろしいですか？')) {
+        return false;
+    }
+});
+
+$(document).on('submit', '.form-logout', function() {
+    if (!confirm('ログアウトしますか？')) {
+        return false;
+    }
+});
+
+$(document).on('submit', '.form-cancel', function() {
+    if (!confirm('キャンセルしますか？')) {
+        return false;
+    }
+});
+
+$('#search_box').find(':input').each(function() {
+    if ($(this).val() != '') {
+        $('#search_box').find('.card-header').removeClass('collapsed');
+        $('#search_box').find('.collapse').addClass('show');
+        return false;
+    }
+});
+
+$(document).on('click', '.form-reset', function() {
+    $(this).closest('form').find(':input').val('').end().find(':checked').prop('checked', false);
+    $(this).closest('form').find('.default').prop('checked', true);
+    $(this).closest('form').find(':radio').checked = true;
+});
+
+$('[data-toggle="tooltip"]').tooltip();
+
+$('ul.vertical-menu').find('li').each(function() {
+    if (window.location.pathname.includes(this.id)) {
+        $(this).closest('ul.vertical-menu > li').addClass('active');
+        $(this).addClass('active').children('a').addClass('active');
+    }
+});
