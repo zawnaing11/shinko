5a272f4082edb81c57791b43639db81a8faeb708
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2024-07-03 13:42:03 +0630

    migration file

---
+++ database/migrations/2024_07_03_121501_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('users');
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('id')->primary()->comment('ID');
+            $table->integer('company_id');
+            $table->foreign('company_id')->references('id')->on(DB::connection('mysql_shinko')->getDatabaseName() . '.company')->onDelete('cascade')->onUpdate('cascade')->comment('企業ID');
+            $table->string('email', length: 191)->comment('Eメールアドレス');
+            $table->char('password', length: 60)->comment('パスワード');
+            $table->string('name', length: 191)->comment('氏名');
+            $table->boolean('is_active')->default(1)->comment('有効/無効');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
