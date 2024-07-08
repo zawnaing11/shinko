5d25f222c202e1872ad0e3d66dac835e4bd00a8b
myothandatoe-mbridge <myo.thanda.toe@frobom.com>
2024-07-08 08:28:47 +0630

    #16 ユーザー編集

---
+++ app/Http/Controllers/User/Api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\User\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Api\UserRequest;
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
+{
+    public function update(UserRequest $request, User $user)
+    {
+        try {
+            // ユーザーを更新
+            $user->fill($request->validated())->save();
+
+        } catch (Exception $e) {
+            logger()->error('$e', [$e->getCode(), $e->getMessage()]);
+            abort(500);
+        }
+
+        return response()->json($user);
+    }
+
+}
